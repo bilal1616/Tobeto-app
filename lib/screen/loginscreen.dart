ef4295921e10d4a5fe89f2f324cebd02f7b932a6
bilal1616 <bilalgonul.1616@gmail.com>
2024-01-16 14:08:43 +0300

    update loginpage

--- lib/screen/loginscreen.dart
+++ lib/screen/loginscreen.dart
@@ -13,7 +13,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController(); // Kayıt için e-posta
+  final TextEditingController _emailController =
+      TextEditingController(); // Kayıt için e-posta
   bool _isPasswordVisible = false;
   bool _isRegisterScreen = false; // Hangi ekranın gösterileceğini kontrol et
 
@@ -42,7 +43,8 @@ class _LoginScreenState extends State<LoginScreen> {
           Center(
             child: Container(
               width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.62, // Konteyner boyutu
+              height:
+                  MediaQuery.of(context).size.height * 0.63, // Konteyner boyutu
               decoration: BoxDecoration(
                   color: Theme.of(context).scaffoldBackgroundColor,
                   borderRadius: BorderRadius.circular(24)),
@@ -54,7 +56,9 @@ class _LoginScreenState extends State<LoginScreen> {
                         MainAxisAlignment.center, // İçeriği ortalama
                     children: [
                       Image.asset(imagePath, width: 150, height: 75),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.02), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.02), // Boşluk ekleme
                       _isRegisterScreen
                           ? TextField(
                               controller: _emailController,
@@ -69,7 +73,9 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                             )
                           : Container(),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.02), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.02), // Boşluk ekleme
                       TextField(
                         controller: _usernameController,
                         keyboardType: TextInputType.name,
@@ -82,7 +88,9 @@ class _LoginScreenState extends State<LoginScreen> {
                           contentPadding: const EdgeInsets.all(8),
                         ),
                       ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.02), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.02), // Boşluk ekleme
                       TextField(
                         controller: _passwordController,
                         obscureText: !_isPasswordVisible,
@@ -106,7 +114,9 @@ class _LoginScreenState extends State<LoginScreen> {
                             ),
                             contentPadding: const EdgeInsets.all(8)),
                       ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.015), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.015), // Boşluk ekleme
                       ElevatedButton(
                         onPressed: () {
                           if (_isRegisterScreen) {
@@ -124,31 +134,78 @@ class _LoginScreenState extends State<LoginScreen> {
                         style: ElevatedButton.styleFrom(
                             minimumSize: Size(double.infinity, 35)),
                       ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.015), // Boşluk ekleme
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          // Google ile giriş yapma işlemleri
-                        },
-                        icon: Icon(FontAwesomeIcons.google,
-                            color: Colors.white), // Google ikonunu kullan
-                        label: const Text("Google ile Giriş Yap"),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColor.favoriteButtonColor, // Google buton rengi
-                            minimumSize: Size(double.infinity, 35)),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.015), // Boşluk ekleme
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // Google ile giriş yapma işlemleri
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColor.favoriteButtonColor,
+                              minimumSize: Size(60, 60),
+                              shape: CircleBorder(),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Icon(FontAwesomeIcons.google,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // GitHub ile giriş yapma işlemleri
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              minimumSize: Size(60, 60),
+                              shape: CircleBorder(),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Icon(FontAwesomeIcons.github,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Apple ile giriş yapma işlemleri
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              minimumSize: Size(60, 60),
+                              shape: CircleBorder(),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Icon(FontAwesomeIcons.apple,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.015), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.015), // Boşluk ekleme
                       TextButton(
                         onPressed: () {
                           setState(() {
                             _isRegisterScreen = !_isRegisterScreen;
                           });
                         },
-                        child: Text(_isRegisterScreen
-                            ? "Giriş Ekranına Dön"
-                            : "Kayıt Ol", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        child: Text(
+                          _isRegisterScreen ? "Giriş Ekranına Dön" : "Kayıt Ol",
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
+                        ),
                       ),
                       const Divider(), // Divider ekleyin
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.01), // Boşluk ekleme
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height *
+                              0.009), // Boşluk ekleme
                       InkWell(
                         onTap: () {},
                         child: const Text("Parolamı Unuttum"),
