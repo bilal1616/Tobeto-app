d99b4ae3431a89fd71193cff1cd22646a36ed2a3
Oya Özcan <oyaozcan98@gmail.com>
2024-01-23 16:10:23 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,41 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tobeto_app/firebase_options.dart';
+import 'package:tobeto_app/screen/drawermainscreen.dart';
 import 'package:tobeto_app/screen/homescreen.dart';
 import 'package:tobeto_app/screen/loginscreen.dart';
 import 'package:tobeto_app/theme/app_theme.dart';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-runApp(MaterialApp(
-    home: StreamBuilder(
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomeScreen();
+            return const LoginScreen();
           }
-          return const LoginScreen();
-        }),
-  ));
+          return const DrawerMainScreen();
+        },
+      ),
+    );
+  }
 }
+
+
