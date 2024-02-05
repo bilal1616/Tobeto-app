0d0071c9a7063a0caec9035991b37a2e9d4a7c1c
bilal1616 <bilalgonul.1616@gmail.com>
2024-02-05 22:08:10 +0300

    examcard-floatactionmenu update

--- lib/widget/home_widget/examcard.dart
+++ lib/widget/home_widget/examcard.dart
@@ -1,117 +1,193 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ExamCard extends StatelessWidget {
-  const ExamCard({Key? key}) : super(key: key);
+class ExamCard extends StatefulWidget {
+  @override
+  _ExamCardState createState() => _ExamCardState();
+}
+
+class _ExamCardState extends State<ExamCard> {
+  final String? userId = FirebaseAuth.instance.currentUser?.uid;
+
+  Future<List<Map<String, dynamic>>> fetchExams() async {
+    if (userId == null) return [];
+    var userDoc =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    var examIds = userDoc.data()?['exam'] as List<dynamic>? ?? [];
+    List<Map<String, dynamic>> exams = [];
+
+    for (var id in examIds) {
+      var examDoc =
+          await FirebaseFirestore.instance.collection('exam').doc(id).get();
+      if (examDoc.exists) {
+        exams.add(examDoc.data()!);
+      }
+    }
+
+    return exams;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(4),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.background, // Beyaz arkaplan rengi
-        borderRadius: BorderRadius.circular(20), // Kutunun köşe yuvarlaklığı
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3), // Daha belirgin gölgelendirme
-            spreadRadius: 2,
-            blurRadius: 8,
-            offset: Offset(0, 4),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            "Sınavlarım",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Sınavlarım",
-              style: TextStyle(
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).textTheme.titleLarge?.color ??
-                    Colors.black,
+        ),
+        FutureBuilder<List<Map<String, dynamic>>>(
+          future: fetchExams(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Text("No exams found.");
+            }
+
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) =>
+                      _buildExamCard(context, snapshot.data![index]),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            SizedBox(
-              height: 200, // Yükseklik ayarı
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: List.generate(3, (index) => _buildExamCard(context)),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildExamCard(BuildContext context, Map<String, dynamic> exam) {
+    return GestureDetector(
+      onTap: () => showExamDetails(context, exam),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Container(
+          width: 180,
+          height: 220,
+          padding: EdgeInsets.all(12),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      exam['title'],
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 12,
+                    ),
+                  ),
+                  Text(
+                    exam['subtitle'],
+                    style: TextStyle(fontSize: 15),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 6,
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.timer_outlined,
+                        size: 21,
+                        color: Colors.purple,
+                      ),
+                      SizedBox(width: 5),
+                      Text(exam['duration'],
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ],
               ),
-            ),
-          ],
+              Positioned(
+                top: 0,
+                right: 0,
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/converted.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildExamCard(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      elevation: 4,
-      color: Theme.of(context).cardColor, // Card arkaplan rengi beyaz
-      child: Container(
-        color: Theme.of(context).cardColor,
-        width: 150, // Card genişliği
-        height: 190, // Card yüksekliği
-        padding: EdgeInsets.all(8.0),
-        child: Stack(
-          children: [
-            Column(
+  void showExamDetails(BuildContext context, Map<String, dynamic> exam) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          heightFactor: 0.7,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Herkes için Kodlama 1D Değerlendirme Sınavı",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                  ),
+                  exam['title'],
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 8),
-                Text(
-                  "Herkes İçin",
-                  style: TextStyle(
-                    color: Theme.of(context).textTheme.bodyLarge?.color ??
-                        Theme.of(context).colorScheme.background,
-                    fontSize: 12,
-                  ),
-                ),
-                Text(
-                  "Kodlama - 1D",
-                  style: TextStyle(
-                    color: Theme.of(context).textTheme.bodyLarge?.color ??
-                        Theme.of(context).colorScheme.background,
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  children: [
-                    Icon(Icons.timer_outlined,
-                        color:
-                            Theme.of(context).colorScheme.primary, // Tema rengi
-                        size: 20),
-                    SizedBox(width: 8.0),
-                    Text(
-                      "45 Dakika",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                      ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      exam['content'],
+                      style: TextStyle(fontSize: 18),
                     ),
-                  ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Raporu Görüntüle',
+                        style: TextStyle(fontSize: 18)),
+                  ),
                 ),
               ],
             ),
-            Positioned(
-                top: 0, right: 0, child: Image.asset("assets/converted.png")),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
